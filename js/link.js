0e43f011b8bcc4aa7ec1c2d93eca05550fbc3ae2
kurtulx <kurtmaestro404@gmail.com>
2025-11-25 02:50:41 +0700

    Update link.js

--- js/link.js
+++ js/link.js
@@ -12,7 +12,8 @@ let selectedProxy = null
 const defaultProxyUrl = "https://raw.githubusercontent.com/FoolVPN-ID/Nautica/refs/heads/main/proxyList.txt"
 
 const serverDomains = [
-  "konz.rj79.dpdns.org"
+  "konz.rj79.dpdns.org",
+  "1.zjzlcloud.dpdns.org"
 ]
 let selectedServerDomain = serverDomains[0] // Default to first domain
 const defaultUUID = "fccdaaae-af39-41a7-9fde-fd32a48278cf"
