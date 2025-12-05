b7cd13c7f7a49ed1e469f640635f2d0602ef9d5c
kurtulx <kurtmaestro404@gmail.com>
2025-12-05 18:18:17 +0700

    Update link.js

--- js/link.js
+++ js/link.js
@@ -12,7 +12,7 @@ let selectedProxy = null
 const defaultProxyUrl = "https://raw.githubusercontent.com/afr0700/ProxyList/refs/heads/main/rawProxyList.txt"
 
 const serverDomains = [
-  "1.rj79.dpdns.org"
+  "x.zjzlcloud.dpdns.org"
 ]
 let selectedServerDomain = serverDomains[0] // Default to first domain
 const defaultUUID = "fccdaaae-af39-41a7-9fde-fd32a48278cf"
